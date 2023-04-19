0a5d73ac7fca086d4d16df52f73e04ce4561a842
Nicolas Mantovani <107324639+vXrayAAA@users.noreply.github.com>
2023-04-19 03:37:44 -0300

    Update config.h

--- main/config.h
+++ main/config.h
@@ -4,7 +4,7 @@
 #define espURL     "http://192.168.4.1"    // Configure a URL.
 #define espURI     "/nik"                 // Configure a URI.
 #define CONFIG_ESP_WIFI_SSID_STA            "PNG"                // SSID da Estacao (STA).
-#define CONFIG_ESP_WIFI_PASSWORD_STA        "41435975"              // Senha da Estacao (STA).
+#define CONFIG_ESP_WIFI_PASSWORD_STA        "//////"   //senha da sua rede          // Senha da Estacao (STA).
 
 #define CONFIG_ESP_WIFI_SSID_AP             "n1k_devboard"              // SSID do AP.
 #define CONFIG_ESP_WIFI_PASSWORD_AP         "abcdefgh"              // Senha do AP.
